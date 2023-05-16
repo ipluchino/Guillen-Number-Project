c4fcb3b37da3ae91703818197beaeb62257f655a
ipluchino <ianlikesmath@gmail.com>
2023-05-16 19:20:06 -0400

    fixed another typo in file name

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.locals.pretty = true;
 app.set('view engine', 'pug');
 
 //Handles the routes dealing with the home page.
-app.use('/', require('./routes/root'));
+app.use('/', require('./Routes/root'));
 
 app.listen(80, () => {
     console.log('Server is running on port 80')
